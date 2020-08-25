1e592f4132ec69a40399ce3a6e51a9babdff69ae
Pranav <P.DoijodeSumanth@tudelft.nl>
2020-08-25 14:20:33 +0200

    Time series prediction

--- preprocess/import_data.py
+++ preprocess/import_data.py
@@ -9,6 +9,7 @@ from os import path
 import numpy as np
 from scipy.signal import savgol_filter
 from sklearn import preprocessing
+import pandas
 
 
 def moving_average(a, n=3):
@@ -16,7 +17,6 @@ def moving_average(a, n=3):
     a_new = a
     for i in range(0, n):
         a_new = np.row_stack((np.zeros(len(a[0])), a_new))
-    #print(a_new)
     ret = np.cumsum(a_new, dtype=np.float32, axis=0)
     ret[n:, :] = ret[n:, :] - ret[:-n, :]
     return ret[n - 1:, :] / n
@@ -83,10 +83,9 @@ def load_sensor_data(path2file=path.abspath("../data/sensor.csv"), num_rows=100)
                                  skiprows=1)
 
     # STEP 4: Some bit of critical think over physics of the problem
-    # and transforming failure_signals into a reliable continuous functions.
-    # Doesn't impact regression as we are adding both LHS and RHS with \bar{x}
+    # Turbomachines are never designed to have a lot of variations in the system
     failure_signals += np.mean(sensor_readings, axis=1)
-    sensor_readings += np.mean(sensor_readings, axis=1).reshape(-1, 1)
+    # sensor_readings += np.mean(sensor_readings, axis=1).reshape(-1, 1)
 
     # STEP 5: Stack to create data matrix
     data_matrix = np.column_stack((timestamps, sensor_readings, failure_signals))
@@ -99,5 +98,14 @@ def load_sensor_data(path2file=path.abspath("../data/sensor.csv"), num_rows=100)
     # STEP 6: A bit controversial to play with actual data
     # BUT filtering the raw signal is in practice useful to weed out noise and is usually done
     data_matrix[:, 1:] = savgol_filter(data_matrix[:, 1:], 3001, 2, axis=0)
+
+    # STEP 7: Generate time series
+    df_fwd = pandas.DataFrame(data=data_matrix[:, 1:-1])
+    df_bwd = pandas.DataFrame(data=data_matrix[:, 1:-1][::-1])
+    df_ewma_fwd = df_fwd.ewm(span=10).mean()
+    df_ewma_bwd = df_bwd.ewm(span=10).mean()
+
+    c = (df_ewma_bwd.values[::-1] + df_ewma_fwd.values) / 2.0
+    data_matrix[:, 1:-1] = c
 
     return data_matrix
