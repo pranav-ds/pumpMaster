d2e86a4c038ee8ed65395c6d07afc86c4d575f41
Pranav <P.DoijodeSumanth@tudelft.nl>
2020-08-25 11:43:49 +0200

    ML module for linear regression

---
+++ ml/__init__.py
@@ -0,0 +1,3 @@
+#  Created by Pranav on 24/08/2020.
+#  Copyright © 2020-2021 Pranav. All rights reserved.
+
---
+++ ml/lr.py
@@ -0,0 +1,64 @@
+#  Created by Pranav on 24/08/2020.
+#  Copyright © 2020-2021 Pranav. All rights reserved.
+
+# Standard Python imports
+from os import path
+
+# Site-packages import
+from sklearn import linear_model
+from sklearn.model_selection import KFold
+from sklearn.metrics import mean_squared_error
+
+from matplotlib import pyplot as plt
+
+
+def build_lr(X_in,
+             y_in,
+             do_cv=True,
+             save_cv_plots=True,
+             path2save=path.abspath("../data/cv_plots"),
+             *args, **kwargs):
+    """
+    This function builds the linear regression model
+    :param X_in            : Data set
+    :param y_in            : Targets
+    :param do_cv           : Do cross validation
+    :param save_cv_plots   : Save CV plots
+    :return: The fit model
+    """
+
+    regr = linear_model.LinearRegression()
+
+    if not do_cv:
+        regr.fit(X_in, y_in)
+        return regr
+
+    else:
+        # KFold splits for validation
+        kf = KFold(n_splits=10)
+        for index, [train_index, test_index] in enumerate(kf.split(X_in, y_in)):
+            y_train = y_in[train_index].reshape(-1, 1)
+            regr.fit(X_in[train_index, :], y_train)
+
+            # Make predictions using the testing set
+            y_pred = regr.predict(X_in[test_index])
+            mse = mean_squared_error(y_in[test_index], y_pred)
+            print("MSE: ", mse)
+
+            if save_cv_plots:
+                # Plot outputs
+                fig = plt.figure()
+                plt.plot(X_in[test_index, 0], y_pred, color='blue', linewidth=3, label="Prediction")
+                plt.plot(X_in[test_index, 0], y_in[test_index], color='red', label="Reality")
+
+                plt.xlabel("Time")
+                plt.ylabel("f(x)")
+                plt.title("CV_" + str(index) + " MSE: %.3e" % (mse,))
+
+                plt.show()
+                fig.savefig(path2save + "/CV_" + str(index) + ".jpg")
+                plt.close()
+
+        return regr
+
+
