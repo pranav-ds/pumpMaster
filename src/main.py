41156afb2aac5bb1fca2006888c74e58c041cb3d
Pranav <P.DoijodeSumanth@tudelft.nl>
2020-08-25 11:44:25 +0200

    A clean entry point to the application

--- src/main.py
+++ src/main.py
@@ -1,31 +1,20 @@
-#%%
+#  Created by Pranav on 24/08/2020.
+#  Copyright © 2020-2021 Pranav. All rights reserved.
 
-# Importing standard libraries
-import numpy as np
-from datetime import datetime
+# Standard Python imports
 from os import path
 
-# STEP 1: Preprocess data to convert time stamps to t ~ [0.0, t_final] in seconds
-str2date = lambda x: datetime.strptime(x.decode('UTF-8'), '%Y-%m-%dT%H:%M:%S')
-timestamps = np.loadtxt(path.abspath("../data/sensor.csv"),
-                        dtype='datetime64[s]',
-                        delimiter=',',
-                        converters={0: str2date, 4: np.float},
-                        max_rows=100,
-                        usecols=(1,),
-                        skiprows=1)
+# pumpMaster module imports
+from preprocess.import_data import load_sensor_data
+from ml.lr import build_lr
 
-#timestamps = timestamps[:, 0]
+if __name__ == "__main__":
+    # Load data
+    data_matrx = load_sensor_data(path.abspath("../data/sensor.csv"), num_rows=None)
 
-# An inefficient but necessary step as numpy.array subtract operaator is broken for dates
-timestamps = np.array([(timestamps[i] - timestamps[0]).item().total_seconds() for i in range(0, len(timestamps))])
+    # Extract features and response from data_matrix
+    X = data_matrx[:, :-1]
+    y = data_matrx[:, -1]
 
-# Now load sensor data
-sensor_readings = np.loadtxt(path.abspath("../data/sensor.csv"),
-                             delimiter=',',
-                             usecols=tuple(range(2,)),
-                             skiprows=1)
-
-print(sensor_readings)
-
-
+    # Build and validate our regression model
+    regr = build_lr(X, y)
