4e7d2b1646748ca0944c9bcba4e5a982eee8f963
Pranav <P.DoijodeSumanth@tudelft.nl>
2020-08-25 11:46:07 +0200

    Setup script

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='pumpMaster',
+    version='1.0.0',
+    packages=['ml', 'src', 'notes', 'preprocess'],
+    url='https://github.com/pranav-ds/pumpMaster',
+    license='All rights reserved',
+    author='pranav',
+    author_email='',
+    description='Predict failures in pumps'
+)
